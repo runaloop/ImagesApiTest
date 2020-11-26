33171b0bb84592b33110b0fa10e10fb19a2773e4
AP <arkaha@gmail.com>
2020-11-26 15:48:39 +0800

    Add: Like status show with likes count;

--- app/src/main/java/com/catp/imagesapitestapp/util/DataBindingAdapters.kt
+++ app/src/main/java/com/catp/imagesapitestapp/util/DataBindingAdapters.kt
@@ -1,6 +1,8 @@
 package com.catp.imagesapitestapp.util
 
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 
@@ -8,3 +10,19 @@ import com.bumptech.glide.Glide
 fun setImage(view: ImageView, imgPath: String) {
     Glide.with(view).load(imgPath).into(view)
 }
+
+@BindingAdapter("setFavorite", "favoriteIcon", "notFavoriteIcon")
+fun setFavorite(
+    view: ImageView,
+    isFavorite: Boolean,
+    favoriteIcon: Drawable,
+    notFavoriteIcon: Drawable
+) {
+    view.setImageDrawable(if (isFavorite) favoriteIcon else notFavoriteIcon)
+}
+
+@BindingAdapter("likesCount")
+fun likesCount(view: TextView, likesCount: Int) {
+    view.text = likesCount.toString()
+}
+
