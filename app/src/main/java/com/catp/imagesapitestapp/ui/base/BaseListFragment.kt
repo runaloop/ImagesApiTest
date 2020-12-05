b90ea0b907c49b0cb2544381000763d52abc37a6
AP <arkaha@gmail.com>
2020-12-05 15:19:55 +0800

    Add: Cat/Dog things to figure out how dagger can work with super classes;

--- app/src/main/java/com/catp/imagesapitestapp/ui/base/BaseListFragment.kt
+++ app/src/main/java/com/catp/imagesapitestapp/ui/base/BaseListFragment.kt
@@ -1,4 +1,4 @@
-package com.catp.imagesapitestapp.ui.home
+package com.catp.imagesapitestapp.ui.base
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,25 +7,39 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.catp.imagesapitestapp.MyRoom
 import com.catp.imagesapitestapp.databinding.FragmentHomeBinding
 import com.catp.imagesapitestapp.di.viewModelWithProvider
-import com.catp.imagesapitestapp.ui.base.BaseListFragment
+import com.catp.imagesapitestapp.ui.home.PhotosAdapter
 import com.google.android.material.snackbar.Snackbar
 import javax.inject.Inject
 import javax.inject.Provider
 
-class HomeFragment @Inject constructor(
-    private val viewModelProvider: Provider<HomeViewModel>,
-    private val adapter: PhotosAdapter,
-    private val myRoom: MyRoom
+open class BaseListFragment<T> @Inject constructor(
+    private val viewModelProvider: Provider<T>,
+    private val adapter: PhotosAdapter
 ) : Fragment() {
+
+
+    //protected val viewModel: T by viewModelWithProvider { viewModelProvider.get() }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        viewModelProvider.get()
         val binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        /*viewModel.errorText.observe(viewLifecycleOwner, Observer {
+            Snackbar.make(binding.root, it, Snackbar.LENGTH_LONG)
+        })
+        binding.recyclerView.let {
+            it.layoutManager = LinearLayoutManager(activity)
+            it.adapter = adapter
+        }
+
+        viewModel.items.observe(viewLifecycleOwner) {
+            adapter.setData(it)
+        }*/
         return binding.root
     }
 }
--- app/src/main/java/com/catp/imagesapitestapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/catp/imagesapitestapp/ui/home/HomeViewModel.kt
@@ -1,97 +1,11 @@
 package com.catp.imagesapitestapp.ui.home
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.catp.imagesapitestapp.data.Repo
-import com.catp.imagesapitestapp.data.model.db.Photo
-import com.catp.imagesapitestapp.util.SingleLiveEvent
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import com.catp.imagesapitestapp.ui.base.BaseListViewModel
 import io.reactivex.rxjava3.disposables.CompositeDisposable
-import io.reactivex.rxjava3.kotlin.addTo
 import javax.inject.Inject
 
 class HomeViewModel @Inject constructor(
-    private val repo: Repo,
-    private val cs: CompositeDisposable
-) :
-    ViewModel() {
-
-
-    private val _errorText = SingleLiveEvent<String>()
-    val errorText = _errorText as LiveData<String>
-
-    private val _loading = MutableLiveData<Boolean>().apply {
-        true
-    }
-    val loading: LiveData<Boolean> = _loading
-
-    private val _items = MutableLiveData<List<Photo>>()
-    val items: LiveData<List<Photo>> = _items
-
-    //TODO: Насколько правильно обновлять состояние ошибки из двух несвязанных мест?
-    init {
-        repo.requestRecentPhotos()
-            .doOnError {
-                _errorText.value = "Network error ${it.message}"
-            }
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe()
-            .addTo(cs)
-
-        repo.getRecentPhotos()
-            .observeOn(AndroidSchedulers.mainThread())
-            .doOnSubscribe {
-                _loading.value = true
-            }
-            .doOnError {
-                _loading.value = false
-                _errorText.value = "Error ${it.message}"
-            }
-            .subscribe { newValues ->
-                _items.value = newValues
-            }
-            .addTo(cs)
-    }
-
-    fun refreshList() {
-        repo.requestRecentPhotos()
-            .doOnError {
-                _errorText.value = "Network error ${it.message}"
-            }
-            .subscribe()
-            .addTo(cs)
-    }
-
-    fun loadMore() {
-        repo.requestRecentPhotos()
-            .doOnError {
-                _errorText.value = "Network error ${it.message}"
-            }
-            .subscribe()
-            .addTo(cs)
-    }
-
-    override fun onCleared() {
-        cs.clear()
-        super.onCleared()
-    }
-
-    fun onLikeClick(photo: Photo) {
-        if (photo.liked) {
-            photo.liked = false
-            photo.likes--
-        } else {
-            photo.liked = true
-            photo.likes++
-        }
-        repo.updatePhoto(photo)
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-
-            },{
-                _errorText.value = "Can't update: ${it.message}"
-            })
-            .addTo(cs)
-    }
-}
+    repo: Repo,
+    cs: CompositeDisposable
+) : BaseListViewModel(repo, cs)
